40d6ddd8545e61debbab4164c6c227f23aba31b1
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2022-01-19 20:30:18 +0000

    replace assignment with uniform (brace) initialisation

--- myApp.cpp
+++ myApp.cpp
@@ -5,9 +5,9 @@ int main(int argc, char** argv)
 {
   std::cout << "Hello!" << std::endl;
 
-  int a = 4;
-  int b = 6;
-  int c = mp::AddTwoNumbers(a, b);
+  int a {4};
+  int b {6};
+  int c {mp::AddTwoNumbers(a, b)};
 
   mp::PrintTwoNumbers(a, b);
   std::cout << "Sum of two numbers:" << c << std::endl;
